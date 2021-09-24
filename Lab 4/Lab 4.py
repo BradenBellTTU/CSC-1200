cc1ac7242b158e7002a8b02bf2c4bd2c4a2e9bfa
Braden Bell <32367797+stu-braden-b@users.noreply.github.com>
2021-09-24 13:36:19 -0500

    Lab 4

--- Lab 4/Lab 4.py
+++ Lab 4/Lab 4.py
@@ -1,13 +1,36 @@
-binary = ""
-inputNumber = int(input("Please enter a number: "))
-if inputNumber < 0:
-    print("Please enter a number greater than 0")
-    input("Press 'Enter' to exit")
-    exit()
+"""
+Braden Bell
+CSC-1200-001
+Prof. Focht
+Sep 24, 2021
 
-while inputNumber != 0:
-    remainder = str(inputNumber % 2)
-    inputNumber //= 2
-    binary = remainder + binary
+Lab 4 - Loops
+"""
 
-print(binary)
+def partOne():
+    binary = ""
+    inputNumber = int(input("Please enter a number: "))
+    if inputNumber < 0:
+        print("\nEnter a number greater than 0!\n")
+        input("Press 'Enter' to exit")
+        exit()
+
+    while inputNumber != 0:
+        remainder = str(inputNumber % 2)
+        inputNumber //= 2
+        binary = remainder + binary
+    print(binary)
+
+
+def partTwo():
+    inputNumber = int(input("Please enter a number: "))
+    inputCharacter = input("Please enter a character: ")
+    output = ""
+
+    for x in range(inputNumber):
+        output = output + inputCharacter + " "
+        print(output)
+
+
+partOne()
+partTwo()
