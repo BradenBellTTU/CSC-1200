58447214b230e5f87536fd71e077f9c9cdd3dec5
Braden Bell <32367797+stu-braden-b@users.noreply.github.com>
2021-09-24 13:49:24 -0500

    Lab 4 - Final

--- Lab 4/Lab 4.py
+++ Lab 4/Lab 4.py
@@ -7,30 +7,37 @@ Sep 24, 2021
 Lab 4 - Loops
 """
 
+def clearScreen():
+    for x in range(10):
+        print("\n")
+#Part 1 - While Loop:
 def partOne():
     binary = ""
-    inputNumber = int(input("Please enter a number: "))
-    if inputNumber < 0:
-        print("\nEnter a number greater than 0!\n")
-        input("Press 'Enter' to exit")
-        exit()
-
+    #Ask user for the number they would like to convert
+    inputNumber = int(input("Please enter a positive integer that you would like to convert to binary: "))
+    #Iterates though inputNumber and create the binary number in a string
     while inputNumber != 0:
         remainder = str(inputNumber % 2)
         inputNumber //= 2
         binary = remainder + binary
-    print(binary)
+    #Final output
+    print("\nYour binary number: " + binary + "\n")
+    input("Press 'enter' to continue to part 2 of lab...")
 
 
 def partTwo():
-    inputNumber = int(input("Please enter a number: "))
-    inputCharacter = input("Please enter a character: ")
     output = ""
-
+    #Ask user for inputs
+    inputNumber = int(input("Please enter the number of lines you would like: "))
+    inputCharacter = input("Please enter a single character you would like to use: ")
+    #Iterate thourgh inputNumber and adds inputCharacter to ouput for every loop
     for x in range(inputNumber):
         output = output + inputCharacter + " "
         print(output)
+    input("Press 'enter' to close the program")
 
-
+#Run the functions and clears the screen in between them
+clearScreen()
 partOne()
+clearScreen()
 partTwo()
