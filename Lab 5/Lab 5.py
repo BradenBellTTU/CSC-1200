1b18eb7dddff8b31a55a2bbbc72bc1ecd2fdc94c
Braden Bell <32367797+stu-braden-b@users.noreply.github.com>
2021-10-06 09:04:21 -0500

    Added comments

--- Lab 5/Lab 5.py
+++ Lab 5/Lab 5.py
@@ -1,5 +1,16 @@
+"""
+Braden Bell
+CSC-1200-001
+Prof. Focht
+Oct 6th, 2021
+
+This is a simple calculator program that has all the operation requirements for Lab 5. I included an ascii art menu for
+user readability and a loop if the user wants to do multiple calculations, along with some basic input checking to prevent errors.
+"""
+
+#Defines the main() function. This function contains the mechanism for asking the user what operation want and calls the specified function
 def main():
-    while(True):
+    while True:
         print("+---------------------+")
         print("|  Python Calculator  |")
         print("+---------------------+")
@@ -12,6 +23,7 @@ def main():
         print("| 7. Exit Program     |")
         print("+---------------------+\n")
         operation = input("Please Select an Operation: ")
+        #Input check. If it fails, tell user the error and loop back to menu. If sucess, then turn input into integer and break loop
         if operation.isalpha() == False:
             operation = int(operation)
             if operation < 8:
@@ -20,6 +32,7 @@ def main():
                 print("Input a number less than 8!")
         else:
             print("Input a number less than 8!")
+    #Checks operation and calls the desired function by the user and loops if it recieves an invalid input
     if operation == 1:
         addition(getData(1),getData(2))
     elif operation == 2:
@@ -33,10 +46,12 @@ def main():
     elif operation == 6:
         mod(getData(1),getData(2))
     elif operation == 7:
+        print("Goodbye")
         exit()
     else:
         print("Invalid input!")
 
+#Defines all the operation functions
 def addition(x,y):
     z = x + y
     return print(str(x) + " + " + str(y) + " = " + str(z))
@@ -61,11 +76,12 @@ def mod(x,y):
     z = x % y
     return print(str(x) + " % " + str(y) + " = " + str(z))
 
-
+#Defines getData and takes x as an input on whether the user should be asked 1st or 2nd input
 def getData(x):
     while(True):
         if x == 1:
             firstNumber = input("First number: ")
+            #Input check
             if firstNumber.isalpha() == False and firstNumber != "":
                 firstNumber = int(firstNumber)
                 return firstNumber
@@ -73,13 +89,17 @@ def getData(x):
                 print("Please enter a number!")
         elif x == 2:
             secondNumber = input("Second number: ")
+            #Input check
             if secondNumber.isalpha() == False and secondNumber != "":
                 secondNumber = int(secondNumber)
                 return secondNumber
             else:
                 print("Please enter a number!")
 
+#Initial run
 main()
+
+#Loop if the user wants to preform another operation
 go = True
 while go == True:
     run = input("Would you like to do another calculation? (y/n)")
