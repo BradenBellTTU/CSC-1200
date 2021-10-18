cdb0655ed5854ada0209d4794068f519fdee06c7
BradenBellTTU <91089671+BradenBellTTU@users.noreply.github.com>
2021-10-18 13:13:45 -0500

    Lab 6 Complete

--- Lab 6/Lab 6.py
+++ Lab 6/Lab 6.py
@@ -3,23 +3,27 @@ def getData():
     songs = []
     user = ""
     print("Please enter the names of your songs or 'exit' to play them")
-    while True:
+    while user.lower() != "exit":
         user = input("")
-        if user == "exit" or user == "Exit":
-            return songs
-        else:
-            songs.append(user) 
+        songs.append(user)
+    return songs
 
+def clearScreen():
+    for x in range(10):
+        print("\n")
           
 def playRandom():
     x = choice(getData())
+    clearScreen()
     return x
 
 def main():
+    clearScreen()
     print("+--------------------------------------+")
     print("| Welcome to the Python Playlist Maker |")
     print("+--------------------------------------+")
-    getData()
-    print("Now Playing: " + playRandom())
+    print("Now Playing: " + playRandom() + "\n")
+    print("|--[]--------------------------------------|\n")
+    print(" Pause ||          Play >           Skip >>  \n")
 
 main()
