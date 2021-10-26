01133ede7f2cbc20da490be8f1b0372172a8c20b
BradenBellTTU <91089671+BradenBellTTU@users.noreply.github.com>
2021-10-26 14:13:22 -0500

    Lab 7 Mostly Complete

--- Lab 7/Lab 7.py
+++ Lab 7/Lab 7.py
@@ -1,12 +1,30 @@
 import os
+import string
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-dictionary = {}
 def main():
+    dictionary = {}
     with open(os.path.join(__location__, 'BeeMovie.txt')) as file:
         for line in file:
             for word in line.split():
-                print(word)
+                dictionary = count(word,dictionary)
+    return dictionary
 
-main()
 
+def count(word,dictionary):
+    word = word.lower()
+    word = word.strip()
+    word = word.translate(str.maketrans('', '', string.punctuation))
+    wordFound = False
+    for x in dictionary:
+        if x == word:
+            dictionary[x] += 1
+            wordFound = True
+    if wordFound == False:
+        dictionary.update({word : 1})
+    return dictionary
+
+print("One moment please...")
+output = main()   
+for x in output:
+    print(x + " : " + str(output[x]))
