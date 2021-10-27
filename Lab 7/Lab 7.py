97e7694151bcdab61b393e8c3b6855b3f06c826c
BradenBellTTU <91089671+BradenBellTTU@users.noreply.github.com>
2021-10-27 08:19:36 -0500

    Added comments

--- Lab 7/Lab 7.py
+++ Lab 7/Lab 7.py
@@ -1,7 +1,10 @@
 import os
 import string
+#Uses the OS library to get the location of BeeMovie.txt. It works as long as the file is in the same dir as the script
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
+"""main() function defined, iterates through the file and stores each word in 'word' for processing, and calls on the count() function
+and ultimately returns 'dictionary'"""
 def main():
     dictionary = {}
     with open(os.path.join(__location__, 'BeeMovie.txt')) as file:
@@ -10,12 +13,15 @@ def main():
                 dictionary = count(word,dictionary)
     return dictionary
 
-
+#Defines count() and imports 'word' and 'dictionary' into the function
 def count(word,dictionary):
+    #word sanitization
     word = word.lower()
     word = word.strip()
+    #removes punctuation using the translate library
     word = word.translate(str.maketrans('', '', string.punctuation))
     wordFound = False
+    #Adds word to dictionary if it is a new word, if it is not, then it incriments the value of the word in the dictionary
     for x in dictionary:
         if x == word:
             dictionary[x] += 1
@@ -24,7 +30,9 @@ def count(word,dictionary):
         dictionary.update({word : 1})
     return dictionary
 
-print("One moment please...")
-output = main()   
+#Calls main() function and lets the user know an output is being processed if it takes time to process
+print("Loading, one moment please...")
+output = main()
+#Iterates through 'output' and prints each one to the user in the desired format
 for x in output:
     print(x + " : " + str(output[x]))
