cf94d0fc7967fc07071ed49403f016d701e51194
BuildTools <unconfigured@null.spigotmc.org>
2021-12-11 21:55:38 -0600

    Added comments and such

--- Program 4/prog4.py
+++ Program 4/prog4.py
@@ -1,17 +1,32 @@
+"""
+Braden Bell
+CSC-1200-001
+Prof. Focht
+Dec 13th, 2021
+
+Progam 4, constructed from the psudocode to the best of my ability.
+It is a game where you must find all treasures.
+"""
+
+
 import os
+#Once again, I had to use this work around to get my file to open in Visual Studio Code. It would not open otherwise
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 import copy
 
+#Clear screen function
 def clear_screen():
     for x in range(0, 20):
         print("\n")
 
+#Prints the house and player position
 def print_house(h,sr,sc):
     th = copy.deepcopy(h)
     th[sr][sc] = "@"
     for i in th:
         print(''.join(str(x) for x in i), end='')
 
+#Extracts the house from the house file and store it in memory
 def build_house():
     print("Please enter the house file: ")
     house_file = input("")
@@ -23,6 +38,7 @@ def build_house():
         line = housefp.readline()
     return myhouse
 
+#Check direction functions
 def check_north(house, row, col):
     if house[row-1][col] == "*":
         return False
@@ -66,14 +82,16 @@ def can_unlock(house, unlocked, row, col):
     else:
         return False
 
-
 def get_key(house, unlocked, row, col):
     if house[row][col] == "0" or house[row][col] == "1" or house[row][col] == "2" or house[row][col] == "3" or house[row][col] == "4":
         unlocked[int(house[row][col])] = True
         house[row][col] = " "
 
+#Main function
 def main():
+    #Keeps track of doors and if they are locked or unlocked
     unlocked = [False, False, False, False, False]
+    #Defines house
     house = build_house()
     #The player's start location
     startrow = 1
@@ -82,9 +100,14 @@ def main():
     tcol = startcol
     num_treasures = 2
     tcount = 0
+    
+    #Main loop
     while tcount < num_treasures:
+        #Display the map and player position to the user
         print_house(house, startrow, startcol)
-        print("Directions: Use WASD to move")
+
+        #Display directions, get command from user, and take appropriate action
+        print("Directions: Use WASD to move N W S E")
         command = input("")
         if command == "w" and check_north(house, trow, tcol) == True:
             trow = startrow-1
@@ -102,6 +125,8 @@ def main():
             break
         else:
             print("You cannot go that way!")
+        
+        #Door functionality
         if is_door(house, trow, tcol) == True:
             if can_unlock(house, unlocked, trow, tcol) == False:
                 print("Sorry, the door is locked! You need to find a key.")
@@ -113,17 +138,19 @@ def main():
         elif get_key(house, unlocked, trow, tcol) == True:
             print("You found a key!")
         
+        #Move player foward
         else:
             startrow = trow
             startcol = tcol
+            #Check for treasure
             if get_treasure(house, trow, tcol) == True:
                 tcount +=1
                 print("You found a treasure!")
                 print("You have " + str(tcount) + " treasures")
-
+        #Check for win
         if tcount == num_treasures:
             print("Congradulations, you collected all the treasures!")
 
 
-
+#Execute main()
 main()
